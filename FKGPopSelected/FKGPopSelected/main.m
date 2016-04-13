055216d7bc2c21822f7a30dc407612756006863f
forkingghost <forkingghost@126.com>
2016-04-13 14:09:36 +0800

    init

---
+++ FKGPopSelected/FKGPopSelected/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FKGPopSelected
+//
+//  Created by forkingghost on 16/4/13.
+//  Copyright © 2016年 forkingghost. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
